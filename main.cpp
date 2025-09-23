be6b529cc5112b39d6bd543c79450d715317d3b9
DougCouttie94 <40539437@live.napier.ac.uk>
2025-09-23 18:27:47 +0100

    working game, without yet adding score tracking, not added load function yet as not understanding role of it

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <SFML/Graphics.hpp>
 
+//define ball attributes
+
+sf::Vector2f ball_velocity;
+bool isPlayer1Serving = false;
+const float initialVelocityX = 100.f; //horizontal velocity
+const float initialVelocityY = 60.f; // vertical velocity
+
+const float velocity_multiplier = 1.1f;
+
 //Assign keyboard values
 const sf::Keyboard::Key controls[4] = {
   sf::Keyboard::W, //Player1 UP
@@ -30,23 +39,93 @@ void init() {
   //Set size and origin of ball
   ball.setRadius(ballRadius);
   ball.setOrigin(ballRadius / 2.f, ballRadius / 2.f); //should be half the ball width and height
-  //reset paddle position
-  paddles[0].setPosition(paddleOffsetWall + paddleSize.x / 2.f, gameHeight / 2.f);
-  paddles[1].setPosition((gameWidth - paddleOffsetWall) - paddleSize.x / 2.f, gameHeight / 2.f);
-  //reset ball position 
-  ball.setPosition(gameWidth / 2.f, gameHeight / 2.f);
+  
+  //set velocity of ball
+  ball_velocity = { (isPlayer1Serving ? initialVelocityX : -initialVelocityX), initialVelocityY };
 }
+
+void reset() {
+	//reset paddle position
+	paddles[0].setPosition(paddleOffsetWall + paddleSize.x / 2.f, gameHeight / 2.f);
+	paddles[1].setPosition((gameWidth - paddleOffsetWall) - paddleSize.x / 2.f, gameHeight / 2.f);
+	//reset ball position 
+	ball.setPosition(gameWidth / 2.f, gameHeight / 2.f);
+}
+
+void load() {
+	ball_velocity = { (isPlayer1Serving ? initialVelocityX : -initialVelocityX), initialVelocityY };
+}
+
 
 void update(float dt) {
 // handle paddle movement
-    float direction = 0.0f;
+    float direction1 = 0.0f;
+	float direction2 = 0.0f;
     if (sf::Keyboard::isKeyPressed(controls[0])) {
-        direction--;
+        direction1--;
     }
     if (sf::Keyboard::isKeyPressed(controls[1])) {
-        direction++;
+        direction1++;
     }
-  paddles[0].move(sf::Vector2f(0.f, direction * paddleSpeed * dt));
+	if (sf::Keyboard::isKeyPressed(controls[2])) {
+		direction2--;
+	}
+	if (sf::Keyboard::isKeyPressed(controls[3])) {
+		direction2++;
+	}
+  paddles[0].move(sf::Vector2f(0.f, direction1 * paddleSpeed * dt));
+  paddles[1].move(sf::Vector2f(0.f, direction2 * paddleSpeed * dt));
+  ball.move(ball_velocity * dt);
+
+  // check ball collision
+  const float bx = ball.getPosition().x;
+  const float by = ball.getPosition().y;
+  if (by > gameHeight) { //bottom wall
+	  // bottom wall
+	  ball_velocity.x *= velocity_multiplier;
+	  ball_velocity.y *= -velocity_multiplier;
+	  ball.move(sf::Vector2f(0.f, -10.f));
+  }
+  else if (by < 0) { //top wall
+	  // top wall
+	  ball_velocity.x *= velocity_multiplier;
+	  ball_velocity.y *= -velocity_multiplier;
+	  ball.move(sf::Vector2f(0.f, 10.f));
+  }
+  else if (bx > gameWidth) {
+	  // right wall
+	  reset();
+  }
+  else if (bx < 0) {
+	  // left wall
+	  reset();
+  }
+  else if (
+	  //ball is inline or behind paddle AND
+	  bx < paddleSize.x + paddleOffsetWall &&
+	  //ball is below top edge of paddle AND
+	  by > paddles[0].getPosition().y - (paddleSize.y * 0.5) &&
+	  //ball is above bottom edge of paddle
+	  by < paddles[0].getPosition().y + (paddleSize.y * 0.5)) {
+		  {
+			  ball_velocity.x *= -velocity_multiplier;
+			  ball_velocity.y *= velocity_multiplier;
+			  ball.move(sf::Vector2f(0.f, 1.f));
+		  }
+  }
+  else if (
+	  //ball is inline or behind paddle AND
+	  bx > (gameWidth - paddleOffsetWall) - paddleSize.x &&
+	  //ball is below top edge of paddle AND
+	  by > paddles[1].getPosition().y - (paddleSize.y * 0.5) &&
+	  //ball is above bottom edge of paddle
+	  by < paddles[1].getPosition().y + (paddleSize.y * 0.5)) {
+		  {
+			  ball_velocity.x *= -velocity_multiplier;
+			  ball_velocity.y *= velocity_multiplier;
+			  ball.move(sf::Vector2f(0.f, 1.f));
+		  }
+  }
 }
 
 void render(sf::RenderWindow &window) {
@@ -65,6 +144,7 @@ int main(){
 	sf::RenderWindow window(sf::VideoMode({gameWidth, gameHeight}), "PONG");
     //initialise and load
 	init();
+	reset();
 	while (window.isOpen()) {
 		static sf::Clock clock;
 		const float dt = clock.restart().asSeconds();
